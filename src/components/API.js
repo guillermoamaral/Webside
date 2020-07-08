b5d671ed3b714549165cb72c81a4e8554db0362c
Guillermo Amaral <Guillermo.Amaral@aucerna.com>
2020-07-08 18:46:30 -0300

    API class, we will see

---
+++ src/components/API.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+class API {
+    constructor(uri){
+        this.baseUri = uri;
+    };
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default API;
