1f786a20b16dde5d9bd2aecc383cffba3728ed1d
Guillermo Amaral <Guillermo.Amaral@aucerna.com>
2020-08-13 17:33:06 -0300

    Progress

---
+++ src/components/tools/TestRunner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TestRunner extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TestRunner;
