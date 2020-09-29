d60ada384f11b76c996ecf29ab89d024aa529774
Guillermo Amaral <Guillermo.Amaral@aucerna.com>
2020-09-29 17:48:50 -0300

    Connection page

---
+++ src/Connect.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Connect extends Component {
+    render() {
+        return (
+            <div>
+                Connect to some image...
+            </div>
+        )
+    }
+}
+
+export default Connect;
