2b8151ba8828f5aad476ca868d1515dee783cd42
Guillermo Amaral <Guillermo.Amaral@aucerna.com>
2020-10-25 20:43:43 -0300

    Progress

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const socketServer = http.createServer(app)
+
+const io = socketio.listen(socketServer)
+
+io.on('connection', socket => {
+  console.log('user connected.')
+  socket.on('NEW_MESSAGE', payload => {
+    io.emit('NEW_MESSAGE_RECEIVED', payload)
+  })
+})
+
+socketServer.listen(4200, () => console.log('App running on port 4200'))
