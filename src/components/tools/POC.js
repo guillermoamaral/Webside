f2567f39a4a7f7a893cfef3e9f02868a623d3ad3
Guillermo Amaral <guillermo.amaral@Quorumsoftware.com>
2023-12-20 20:26:03 -0300

    POC changes to test SystemBrowser

--- src/components/tools/POC.js
+++ src/components/tools/POC.js
@@ -1,19 +1,13 @@
 import React from "react";
 import Tool from "./Tool";
-import SearchList2 from "../controls/SearchList2";
-import CustomList from "../controls/CustomList";
-import { Paper } from "@mui/material";
+import SystemBrowser from "./SystemBrowser";
 
 //This is is a component to test ideas in a tool-like tab.
 class POC extends Tool {
+
 	render() {
 		return (
-			<div>
-				<SearchList2 options={["aa", "ab", "ac", "ad", "ae"]} />
-				<Paper variant="outlined" sx={{ height: 300 }}>
-					<CustomList items={[1, 2, 3, 4]} />
-				</Paper>
-			</div>
+			<SystemBrowser showPackages={true} />
 		);
 	}
 }
