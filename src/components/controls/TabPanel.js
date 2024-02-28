ca3b8bf28848755bbd3740c66d9cba62196a64e6
Guillermo Amaral <guillermo.amaral@Quorumsoftware.com>
2024-02-28 16:26:22 -0300

    TabPanel padding top fix

--- src/components/controls/TabPanel.js
+++ src/components/controls/TabPanel.js
@@ -7,7 +7,7 @@ class TabPanel extends Component {
 		const { id, children, visible, ...other } = this.props;
 		return (
 			<div role="tabpanel" hidden={!visible} id={id} {...other}>
-				<Box style={{ height: "100%" }}>
+				<Box pt={1} style={{ height: "100%" }}>
 					{children}
 				</Box>
 			</div>
