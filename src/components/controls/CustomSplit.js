6cd0fb44ede50e1a035231fdfcf9af6544cd31b0
Guillermo Amaral <guillermo.amaral@Quorumsoftware.com>
2024-04-03 11:00:03 -0300

    CustomSplit style change

--- src/components/controls/CustomSplit.js
+++ src/components/controls/CustomSplit.js
@@ -7,28 +7,30 @@ class CustomSplit extends PureComponent {
 			<Split
 				{...this.props}
 				//Not sure if this is the best option... perhaps removing this customization produces better results
-				// renderBar={({ onMouseDown, ...props }) => {
-				// 	return (
-				// 		<div
-				// 			{...props}
-				// 			style={{
-				// 				boxShadow: "none",
-				// 				background: "transparent",
-				// 			}}
-				// 		>
-				// 			<div
-				// 				onMouseDown={onMouseDown}
-				// 				style={{
-				// 					backgroundColor: "default",
-				// 					boxShadow: "none",
-				// 				}}
-				// 			/>
-				// 		</div>
-				// 	);
-				// }}
+				renderBar={({ onMouseDown, ...props }) => {
+					return (
+						<div
+							{...props}
+							style={{
+								boxShadow: "none",
+								backgroundColor: "default",
+							}}
+						>
+							<div
+								onMouseDown={onMouseDown}
+								style={{
+									backgroundColor: "transparent",
+									boxShadow: "none",
+								}}
+							/>
+						</div>
+					);
+				}}
 				style={{
 					height: "100%",
 					width: "100%",
+					// boxShadow: "none",
+					// background: "transparent",
 				}}
 			>
 				{this.props.children}
