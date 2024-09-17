2c61f5efdfea7464b71cde7923dff26062c2e778
Guillermo Amaral <guillermo.amaral@Quorumsoftware.com>
2024-09-17 20:09:40 -0300

    Add StAST model with handy services to traverse the AST, find nodes, etc.

---
+++ src/model/StAST.js
@@ -0,0 +1,76 @@
+class StAST extends Object {
+	constructor() {
+		super();
+		this.type = "Method";
+		this.label = "Undefined";
+		this.start = 0;
+		this.end = 0;
+		this.children = [];
+	}
+
+	fromJson(json) {
+		this.type = json.type;
+		this.label = json.label;
+		this.start = json.start;
+		this.end = json.end;
+		this.children = json.children;
+	}
+
+	traverse(node, block) {
+		block(node);
+		if (node.children) {
+			node.children.forEach((n) => {
+				this.traverse(n, block);
+			});
+		}
+	}
+
+	nodeAt(offset) {
+		let node;
+		this.traverse(this, (n) => {
+			if (
+				n.start <= offset &&
+				offset <= n.end &&
+				(!node || (node.start <= n.start && n.end <= node.end))
+			) {
+				node = n;
+			}
+		});
+		return node;
+	}
+
+	nodesSatisfying(condition) {
+		const nodes = [];
+		this.traverse(this, (node) => {
+			if (condition(node)) nodes.push(node);
+		});
+		return nodes;
+	}
+
+	selectorInRage(range) {
+		let node;
+		this.traverse(this, (n) => {
+			if (
+				n.type === "Selector" &&
+				range.from <= n.start &&
+				n.end <= range.to &&
+				(!node || (n.start <= node.start && node.end <= n.end))
+			) {
+				node = n;
+			}
+		});
+		return node;
+	}
+
+	selectors() {
+		const selectors = [];
+		this.nodesSatisfying((node) => node.type === "Selector").forEach(
+			(node) => {
+				if (!selectors.includes(node.value)) selectors.push(node.value);
+			}
+		);
+		return selectors;
+	}
+}
+
+export default StAST;
